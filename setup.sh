60281f5411dfc4260683d7b53b1e32e752a1fcd5
BrianCarmona36 <brian.carmona@itglobers.com>
2024-06-21 20:03:00 -0500

    Save changes

--- setup.sh
+++ setup.sh
@@ -6,12 +6,18 @@ DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3)
 # Exportar el ID del grupo Docker como variable de entorno
 export DOCKER_GROUP_ID
 
-# Levantar los servicios con Docker Compose
-docker-compose up -d
+# Detener los servicios actuales
+docker-compose down
 
-# Añadir el grupo Docker con el ID del host dentro del contenedor
-docker exec -u root jenkins groupadd -g $DOCKER_GROUP_ID docker || true
-docker exec -u root jenkins usermod -aG docker jenkins || true
+# Levantar los servicios con Docker Compose
+docker-compose up -d --build
+
+# Añadir el grupo Docker con el ID del host dentro del contenedor, si no existe
+docker exec -u root integracioncontinua-jenkins-1 sh -c "getent group docker || groupadd -g $DOCKER_GROUP_ID docker"
+
+# Añadir el usuario jenkins al grupo docker
+docker exec -u root integracioncontinua-jenkins-1 usermod -aG docker jenkins
 
 # Reiniciar el contenedor Jenkins para aplicar los cambios
-docker restart jenkins
+docker restart integracioncontinua-jenkins-1
+# 194cc34bd6ae4219a7be5256a7c4ba37
